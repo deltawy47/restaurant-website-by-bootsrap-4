471f22bac3a87ad28871753637b8bb03bf11387f
abdelrahman sm <abdomahalawy01@gmail.com>
2022-10-11 20:06:14 +0200

    finol

---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = function (grunt) {
+    'use strict';
+
+    module.exports = function (grunt) {
+        // Time how long tasks take. Can help when optimizing build times
+        require('time-grunt')(grunt);
+    
+        // Automatically load required Grunt tasks
+        require('jit-grunt')(grunt);
+    
+        // Define the configuration for all the tasks
+        grunt.initConfig({
+            sass: {
+                dist: {
+                    files: {
+                        'css/styles.css': 'css/styles.scss'
+                    }
+                }
+            }
+        });
+    
+        grunt.registerTask('css', ['sass']);
+    
+    };  grunt.initConfig({
+
+  });
+};
